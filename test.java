0910f268b8d0e2d904e10bdf5e3417344a6a88ad
mvkris2 <krishna.malkari@gmail.com>
2026-05-10 01:20:26 -0400

    test

---
+++ test.java
@@ -0,0 +1,7 @@
+/generate code for reverse a string in javapublic class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String reversedStr = reverseString(str);
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reversedStr);
+    }                   
