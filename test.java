53a0e48b45805640c2abbf79ae34d3d7a54447d2
mvkris2 <krishna.malkari@gmail.com>
2026-05-10 01:30:54 -0400

    Add multiple implementations of string reversal in test.java

--- test.java
+++ test.java
@@ -4,4 +4,34 @@
         String reversedStr = reverseString(str);
         System.out.println("Original String: " + str);
         System.out.println("Reversed String: " + reversedStr);
-    }                   
+    }                   '
+    
+    '
+    /generate code for reverse a string in javapublic class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String reversedStr = reverseString(str);
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reversedStr);
+    }                   '
+    
+    '
+    /generate code for reverse a string in javapublic class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String reversedStr = reverseString(str);
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reversedStr);
+    }                   '
+    
+    '
+    /generate code for reverse a string in javapublic class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String reversedStr = reverseString(str);
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reversedStr);
+    }                   '
+    
+    '
+    
